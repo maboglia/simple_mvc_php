19d44f133d208a50560064bee2ffef5a03d4310a
maboglia <mauro.bogliaccino@gmail.com>
2023-07-06 17:39:33 +0200

    agg db

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -7,6 +7,12 @@ class Controller{
         require_once '../app/models/' .  $model . '.php';
         return new $model();
     }
+
+    protected function dao($dao)
+    {
+        require_once '../app/dao/' .  $dao . '.php';
+        return new $dao();
+    }
     
     public function view($view, $data = [])
     {
